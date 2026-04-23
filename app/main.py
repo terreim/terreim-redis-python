1aac409e9f44c446ecddaa91810eea41be94bea8
duylt <duy.ltw4424@gmail.com>
2026-04-23 16:01:11 +0700

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -3,9 +3,10 @@ import socket  # noqa: F401
 
 def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
+    conn, addr = server_socket.accept()
+
     try:
         while True:
-            conn, addr = server_socket.accept()
 
             data = conn.recv(1024)
             if not data:
@@ -17,6 +18,8 @@ def main():
 
     except KeyboardInterrupt:
         print("Shutting down server...")
+        conn.close()
+        server_socket.close()
 
 if __name__ == "__main__":
     main()
