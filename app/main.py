63f4e6aac96399866a38eed7b396fc6958439500
duylt <duy.ltw4424@gmail.com>
2026-04-23 15:56:16 +0700

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -2,12 +2,18 @@ import socket  # noqa: F401
 
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-    
-    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
+    server_socket = socket.create_server(("localhost", 6379), reuse_address=True)
+    conn, addr = server_socket.accept()
 
+    data = conn.recv(1024)
+    if not data:
+        return
+
+    print(f"Received data: {data}")
+    response = b"+PONG\r\n"
+    conn.sendall(response)
+
+    conn.close()
 
 if __name__ == "__main__":
     main()
