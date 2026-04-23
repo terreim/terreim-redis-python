ba859ebe3286be1c0319c139bfa50a739b45503b
duylt <duy.ltw4424@gmail.com>
2026-04-23 15:59:38 +0700

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -3,17 +3,20 @@ import socket  # noqa: F401
 
 def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    conn, addr = server_socket.accept()
+    try:
+        while True:
+            conn, addr = server_socket.accept()
 
-    data = conn.recv(1024)
-    if not data:
-        return
+            data = conn.recv(1024)
+            if not data:
+                return
 
-    print(f"Received data: {data}")
-    response = b"+PONG\r\n"
-    conn.sendall(response)
+            print(f"Received data: {data}")
+            response = b"+PONG\r\n"
+            conn.sendall(response)
 
-    conn.close()
+    except KeyboardInterrupt:
+        print("Shutting down server...")
 
 if __name__ == "__main__":
     main()
